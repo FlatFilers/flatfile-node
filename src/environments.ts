b4424c879105b45c65cba30a04be4c65a40bd3af
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2023-02-22 22:08:19 +0000

    Release 0.1.6

--- src/environments.ts
+++ src/environments.ts
@@ -3,7 +3,7 @@
  */
 
 export const FlatfileEnvironment = {
-    Production: "https://api.flatfile.com/v1",
+    Production: "https://api.x.flatfile.com/v1",
 } as const;
 
 export type FlatfileEnvironment = typeof FlatfileEnvironment.Production;
