f977766bd52d4708bcab2f5d81db58d8ca70c17b
dsinghvi <deep@usebirch.com>
2023-04-23 16:44:24 -0700

    move auth logic to derived FlatFileClient class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * as Flatfile from "./api";
-export { FlatfileClient } from "./Client";
+export { FlatfileClient } from "./wrapper/FlatfileClient";
 export { FlatfileEnvironment } from "./environments";
 export { FlatfileError, FlatfileTimeoutError } from "./errors";
