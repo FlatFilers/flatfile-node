5fe5e5e5eb117d60d260eb58c2b36f9e89d981a3
Deep Singhvi <deep@usebirch.com>
2023-04-23 19:00:41 -0700

    remove clientId/clientSecret auth (#7)
    * the client no longer needs to accept client id and secret
    
    * update readme
    
    * call it apiKey instead of token

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * as Flatfile from "./api";
-export { FlatfileClient } from "./wrapper/FlatfileClient";
+export { FlatfileClient } from "./Client";
 export { FlatfileEnvironment } from "./environments";
 export { FlatfileError, FlatfileTimeoutError } from "./errors";
