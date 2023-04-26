30a928571e2c8cb5a0a7d316ec9a244aa09d9f91
David Boskovic <david@flatfile.com>
2023-04-26 09:26:57 -0500

    add default export

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export * as Flatfile from "./api";
 export { FlatfileClient } from "./wrapper/FlatfileClient";
 export { FlatfileEnvironment } from "./environments";
 export { FlatfileError, FlatfileTimeoutError } from "./errors";
+
+export default new FlatfileClient()
