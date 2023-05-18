35435ce6a30cca8984d7fc13f2cdcea9f34d4733
David Boskovic <david@flatfile.com>
2023-05-17 21:04:22 -0400

    add environment variables for onprem

--- src/wrapper/FlatfileClient.ts
+++ src/wrapper/FlatfileClient.ts
@@ -16,17 +16,20 @@ export class FlatfileClient extends FernClient {
 
     constructor(options: FlatfileClient.Options = {}) {
         super({
-            environment: options.environment,
-            token:
-                options.token != null
-                    ? options.token
-                    : () => {
-                          const token = process?.env?.FLATFILE_API_KEY || process?.env?.FLATFILE_BEARER_TOKEN;
-                          if (token == undefined) {
-                              throw new Error("FLATFILE_API_KEY and FLATFILE_BEARER_TOKEN were both undefined");
-                          }
-                          return token;
-                      },
+            environment: options.environment ?? () => {
+                const url = process?.env?.FLATFILE_API_URL || process?.env?.AGENT_INTERNAL_URL;
+                if (url == undefined) {
+                    throw new Error("FLATFILE_API_URL and AGENT_INTERNAL_URL were both undefined");
+                }
+                return url;
+            },
+            token: options.token ?? () => {
+                const token = process?.env?.FLATFILE_API_KEY || process?.env?.FLATFILE_BEARER_TOKEN;
+                if (token == undefined) {
+                    throw new Error("FLATFILE_API_KEY and FLATFILE_BEARER_TOKEN were both undefined");
+                }
+                return token;
+            },
         });
     }
 }
