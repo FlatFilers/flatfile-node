e2c841a7fefd8d36db4c83547fe1e496c6dcad96
David Boskovic <david@flatfile.com>
2023-05-17 21:10:46 -0400

    fix: syntax

--- src/wrapper/FlatfileClient.ts
+++ src/wrapper/FlatfileClient.ts
@@ -1,7 +1,6 @@
 import { FlatfileClient as FernClient } from "../Client";
 import * as environments from "../environments";
 import * as core from "../core";
-import * as errors from "../errors";
 import urlJoin from "url-join";
 
 export declare namespace FlatfileClient {
@@ -16,20 +15,20 @@ export class FlatfileClient extends FernClient {
 
     constructor(options: FlatfileClient.Options = {}) {
         super({
-            environment: options.environment ?? () => {
+            environment: options.environment ?? (() => {
                 const url = process?.env?.FLATFILE_API_URL || process?.env?.AGENT_INTERNAL_URL;
                 if (url == undefined) {
                     throw new Error("FLATFILE_API_URL and AGENT_INTERNAL_URL were both undefined");
                 }
-                return url;
-            },
-            token: options.token ?? () => {
+                return urlJoin(url, 'v1');
+            }),
+            token: options.token ?? (() => {
                 const token = process?.env?.FLATFILE_API_KEY || process?.env?.FLATFILE_BEARER_TOKEN;
                 if (token == undefined) {
                     throw new Error("FLATFILE_API_KEY and FLATFILE_BEARER_TOKEN were both undefined");
                 }
                 return token;
-            },
+            }),
         });
     }
 }
