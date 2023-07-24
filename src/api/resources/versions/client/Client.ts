3e73a84a2be5e63d6b09be12703553bea4b72cbe
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2023-07-24 15:00:44 +0000

    Release 1.5.19

--- src/api/resources/versions/client/Client.ts
+++ src/api/resources/versions/client/Client.ts
@@ -43,7 +43,7 @@ export class Versions {
                 "X-Disable-Hooks": "true",
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@flatfile/api",
-                "X-Fern-SDK-Version": "1.5.18",
+                "X-Fern-SDK-Version": "1.5.19",
             },
             contentType: "application/json",
             body: await serializers.VersionsPostRequestBody.jsonOrThrow(request, { unrecognizedObjectKeys: "strip" }),
