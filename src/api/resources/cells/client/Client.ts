a4db1338111f524aa7e5d7cacbb234cf516cfec9
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2023-10-10 21:55:43 +0000

    Release 1.5.30

--- src/api/resources/cells/client/Client.ts
+++ src/api/resources/cells/client/Client.ts
@@ -95,7 +95,7 @@ export class Cells {
                 "X-Disable-Hooks": "true",
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@flatfile/api",
-                "X-Fern-SDK-Version": "1.5.29",
+                "X-Fern-SDK-Version": "1.5.30",
             },
             contentType: "application/json",
             queryParameters: _queryParams,
--- src/api/resources/versions/client/Client.ts
+++ src/api/resources/versions/client/Client.ts
@@ -43,7 +43,7 @@ export class Versions {
                 "X-Disable-Hooks": "true",
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@flatfile/api",
-                "X-Fern-SDK-Version": "1.5.29",
+                "X-Fern-SDK-Version": "1.5.30",
             },
             contentType: "application/json",
             body: await serializers.VersionsPostRequestBody.jsonOrThrow(request, { unrecognizedObjectKeys: "strip" }),
