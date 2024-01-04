1ccf19dfdb59ea8f1f0993644334e67a9cc05cf0
Carl Brugger <carl.brugger@flatfile.io>
2024-01-03 19:38:18 -0600

    fix: re-add missing types

--- src/api/resources/mapping/types/index.ts
+++ src/api/resources/mapping/types/index.ts
@@ -9,3 +9,8 @@ export * from "./MappingRuleOrConfig";
 export * from "./CreateMappingRulesRequest";
 export * from "./MappingRuleResponse";
 export * from "./MappingRulesResponse";
+export * from "./GetFieldWeightsRequest";
+export * from "./GetEnumWeightsRequest";
+export * from "./SourceDestinationWeight";
+export * from "./SourceDestinationWeights";
+export * from "./SourceDestinationWeightsResponse";
